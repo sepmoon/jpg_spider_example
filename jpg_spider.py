74ab63d3ed045d2ec5432f9e7a69b62f067ba38b
kevin <kevin@kevindeMac-Pro.local>
2019-04-22 17:26:49 +0800

    优化代码

--- jpg_spider.py
+++ jpg_spider.py
@@ -3,6 +3,7 @@
 
 
 from concurrent.futures import ThreadPoolExecutor, wait
+from urllib3.util.url import parse_url
 import certifi  # 证书认证
 import urllib3
 import re
@@ -19,14 +20,37 @@ class GetPicDownload:
             user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:66.0) Gecko/20100101 Firefox/66.0')
         self.regulars = re.compile(r'src=.+\.jpg|src=.+\.png')
         self.con = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())  # 设置证书认证
+        self.urls = None
+
+    @staticmethod
+    def _url_testing(url):
+        scheme_ = parse_url(url).scheme
+        if scheme_ == 'http' or scheme_ == 'https':
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def _get_pic_name(url):
+        return re.split(r'//|/', url)[-1].split('.')
+
+    def _get_pic(self, url):
+        if url:
+            result = self.con.request('GET', url, headers=self.header)
+            if result.data:
+                pic_name = self._get_pic_name(url)
+                fo = open('{}-.{}'.format(pic_name[0], pic_name[1]), 'wb')
+                fo.write(result.data)
+                fo.close()
+        else:
+            print("没有可用下载链接")
 
     def get_pic_url(self, url):
         """
         获取目标URL可下载列表
         """
-        if re.search(r'http://|https://', str(url)):
-            if len(re.split(r'://|/', str(url))) == 2:
-                url = str(url) + '/'
+        if self._url_testing(url):
+            self.urls = parse_url(url)
             lists_ = []
             new_con = self.con.request('GET', url, headers=self.header)
             con_data = new_con.data.decode('utf-8')
@@ -34,11 +58,10 @@ class GetPicDownload:
             for num in range(len(list_)):
                 try:
                     list_str = list_[num].split('="')[1]
-                    if re.search(r'http://|https://', list_str):
+                    if self._url_testing(list_str):
                         lists_.append("{}".format(list_str))
                     else:
-                        re_url = re.split(r'://|/', url)
-                        lists_.append("{s1}://{s2}{s3}".format(s1=re_url[0], s2=re_url[1], s3=list_str))
+                        lists_.append("{s1}://{s2}{s3}".format(s1=self.urls.scheme, s2=self.urls.host, s3=list_str))
                 except BaseException:
                     pass
             lists_ = list(dict.fromkeys(lists_).keys())  # 提取图片链接去重，返回一个列表
@@ -58,21 +81,10 @@ class GetPicDownload:
             wait(all_task)
             print('All Download Finish.')
 
-    def _get_pic(self, url):
-        if url:
-            pic_name = re.split(r'//|/', url)[-1].split('.')
-            fo = open('{}-.{}'.format(pic_name[0], pic_name[1]), 'wb')
-            dpic = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
-            result = dpic.request('GET', url, headers=self.header)
-            fo.write(result.data)
-            fo.close()
-        else:
-            print("没有可用下载链接")
-
 
 if __name__ == '__main__':
     start_time = time.time()
     run = GetPicDownload()
-    run.download_pic('http://www.nipic.com/')
+    run.download_pic('http://www.nipic.com')
     finish_time = time.time() - start_time
     print(finish_time)
